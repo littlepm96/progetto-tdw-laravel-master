791ac812a904e15f839bafef44138637ffb19667
littlepm96 <poscentemarco@hotmail.it>
2021-10-05 18:21:27 +0200

    logo 2 in barra navigazione

--- resources/views/layouts/header_3.blade.php
+++ resources/views/layouts/header_3.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <title>Beerify Story</title>
+    <title>Birrante Story</title>
     <link rel="icon" type="image/png" href="../public/img/wheat.png" />
     <link href="{{ asset('new_template/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ asset('new_template/css/fakeLoader.css')}}" rel="stylesheet">
